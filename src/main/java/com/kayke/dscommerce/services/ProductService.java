c90f184526f27ba907b764bd1febcdfbd0023b3a
Kayke Moreira <kayke.allogins@gmail.com>
2026-02-15 20:31:47 -0300

    GET Products paged

--- src/main/java/com/kayke/dscommerce/services/ProductService.java
+++ src/main/java/com/kayke/dscommerce/services/ProductService.java
@@ -1,8 +1,9 @@
 package com.kayke.dscommerce.services;
-
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,6 +24,14 @@ public class ProductService {
         Product product = result.get();
 
         return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+
+        Page<Product> result = productRepository.findAll(pageable);
+
+        return result.map(product -> new ProductDTO(product));
     }
 
 }
